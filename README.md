712e1e2815ecc4364361372001d022ec18ed17ca
rbhushan <rbhushan@global.sial.com>
2021-05-11 14:59:00 +0530

    css changes

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-# maersk_ui_assignment
+# Maersk UI Assignment
+> This assigment contain Sort and Shuffle module.
+
+## Built With
+>**JavaScript**,**HTML**,**CSS**
+
+## Getting Started
+> Clone the repository.
+> Open index.html using any browser.
+
+## Feature
+> Use Shuffle button to randomly shuffle the cards from 1-9.
+> Use Sort button to arrange the card in ascending order.
--- scripts/sortShuffle.js
+++ scripts/sortShuffle.js
@@ -2,32 +2,34 @@ let numArray = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 let cardColor = ['#6F98A8', '#2B8EAD', '#2F454E', '#2B8EAD', '#2F454E', '#BFBFBF', '#BFBFBF', '#6F98A8', '#2F454E'];
 
 // Function to generate card dynamically
-const createCard = () => {
+const createCards = () => {
     numArray.forEach((element, index) => {
         let card = document.createElement('div');
         card.id = 'card'+ index;
         card.innerText = numArray[index];
         card.style.background = cardColor[index];
-        card.style.borderLeft = cardColor[index] + "10px solid";
+        card.style.borderLeft = cardColor[index] + " 10px solid";
         document.getElementById('grid-container').appendChild(card);
     });
 }
 
+// Function to update the card after shuffle or sort 
 const updateCard = () => {
     for (let i = 0; i < numArray.length; i++) {
         let cardID = "card"+ i;
         let card = document.getElementById(cardID);
         card.style.background = cardColor[i];
-        card.style.borderLeft = cardColor[i] + "10px solid";
+        card.style.borderLeft = cardColor[i] + " 10px solid";
         document.getElementById(cardID).style.order = numArray[i];
     }
 };
-
+// Funcion to shuffle the cards
 const shuffleCard = () => {
     numArray.sort(() => Math.random()- .5);
     updateCard();
 };
 
+// This method is used to check whether the cards are already sorted
 const isArraySorted=(arr)=>{
     let isSorted=true;
     for(let i=0;i<arr.length;i++){
@@ -38,6 +40,7 @@ const isArraySorted=(arr)=>{
     return isSorted;
 };
 
+// This method is used to check and sort cards
 const sortCard = () => {
     if(isArraySorted(numArray)){
         alert("Card is already sorted")
